009692987d744d1e0f5e4e4c368750910d1caaed
micro-hawk <dasvikas0071@gmail.com>
2023-04-24 11:00:09 +0530

    initialize student server for webisssh

---
+++ student/src/main/java/com/microhawk/student/SampleStudentServerApplication.java
@@ -0,0 +1,13 @@
+package com.microhawk.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleStudentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleStudentServerApplication.class, args);
+	}
+
+}
---
+++ student/src/test/java/com/microhawk/student/SampleStudentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microhawk.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleStudentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
